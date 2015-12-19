b8301cd124faef7d366f0905c677b2dd4f995625
perlancar (@pc-home) <perlancar@gmail.com>
2015-12-19 22:10:11 +0700

    Initial commit

---
+++ lib/Text/Table/Any.pm
@@ -0,0 +1,179 @@
+package Text::Table::Any;
+
+# DATE
+# VERSION
+
+#IFUNBUILT
+use 5.010001;
+use strict;
+use warnings;
+#END IFUNBUILT
+
+sub _encode {
+    my $val = shift;
+    $val =~ s/([\\"])/\\$1/g;
+    "\"$val\"";
+}
+
+sub table {
+    my %params = @_;
+    my $rows = $params{rows} or die "Must provide rows!";
+
+    my $backend = $params{backend} || 'Text::Table::Tiny';
+
+    if ($backend eq 'Text::Table::Tiny') {
+        require Text::Table::Tiny;
+        return Text::Table::Tiny::table(
+            rows => $rows, header_row => $params{header_row}) . "\n";
+    } elsif ($backend eq 'Text::Table::Org') {
+        require Text::Table::Org;
+        return Text::Table::Org::table(
+            rows => $rows, header_row => $params{header_row});
+    } elsif ($backend eq 'Text::Table::CSV') {
+        require Text::Table::CSV;
+        return Text::Table::CSV::table(
+            rows => $rows);
+    } elsif ($backend eq 'Text::ANSITable') {
+        require Text::ANSITable;
+        my $t = Text::ANSITable->new(
+            use_utf8 => 0,
+            use_box_chars => 0,
+            use_color => 0,
+            columns => $rows->[0],
+            border_style => 'Default::single_ascii',
+        );
+        # XXX pick an appropriate border style when header_row=0
+        $t->add_row($rows->[$_]) for 1..@$rows-1;
+        return $t->draw;
+    } elsif ($backend eq 'Text::ASCIITable') {
+        require Text::ASCIITable;
+        my $t = Text::ASCIITable->new();
+        $t->setCols(@{ $rows->[0] });
+        $t->addRow(@{ $rows->[$_] }) for 1..@$rows-1;
+        return "$t";
+    } elsif ($backend eq 'Text::FormatTable') {
+        require Text::FormatTable;
+        my $t = Text::FormatTable->new(join('|', ('l') x @{ $rows->[0] }));
+        $t->head(@{ $rows->[0] });
+        $t->row(@{ $rows->[$_] }) for 1..@$rows-1;
+        return $t->render;
+    } elsif ($backend eq 'Text::MarkdownTable') {
+        require Text::MarkdownTable;
+        my $out = "";
+        my $t = Text::MarkdownTable->new(file => \$out);
+        my $fields = $rows->[0];
+        foreach (1..@$rows-1) {
+            my $row = $rows->[$_];
+            $t->add( {
+                map { $fields->[$_] => $row->[$_] } 0..@$fields-1
+            });
+        }
+        $t->done;
+        return $out;
+    } elsif ($backend eq 'Text::Table') {
+        require Text::Table;
+        my $t = Text::Table->new(@{ $rows->[0] });
+        $t->load(@{ $rows }[1..@$rows-1]);
+        return $t;
+    } elsif ($backend eq 'Text::TabularDisplay') {
+        require Text::TabularDisplay;
+        my $t = Text::TabularDisplay->new(@{ $rows->[0] });
+        $t->add(@{ $rows->[$_] }) for 1..@$rows-1;
+        return $t->render . "\n";
+    } else {
+        die "Unknown backend '$backend'";
+    }
+}
+
+1;
+#ABSTRACT: Generate text table using one of several backends
+
+=head1 SYNOPSIS
+
+ use Text::Table::Any;
+
+ my $rows = [
+     # header row
+     ['Name', 'Rank', 'Serial'],
+     # rows
+     ['alice', 'pvt', '123456'],
+     ['bob',   'cpl', '98765321'],
+     ['carol', 'brig gen', '8745'],
+ ];
+ print Text::Table::Any::table(rows => $rows, header_row => 1,
+                               backend => 'Text::Table::Tiny');
+
+
+=head1 DESCRIPTION
+
+This module provides a single function, C<table>, which formats a
+two-dimensional array of data as text table, using one of several available
+backends. The interface is modelled after L<Text::Table::Tiny> (0.3);
+Text::Table::Tiny also happens to be the default backend.
+
+The example shown in the SYNOPSIS generates the following table:
+
+ +-------+----------+----------+
+ | Name  | Rank     | Serial   |
+ | alice | pvt      | 123456   |
+ | bob   | cpl      | 98765321 |
+ | carol | brig gen | 8745     |
+ +-------+----------+----------+
+
+
+=head1 FUNCTIONS
+
+=head2 table(%params) => str
+
+
+=head2 OPTIONS
+
+The C<table> function understands these arguments, which are passed as a hash.
+
+=over
+
+=item * rows (aoaos)
+
+Required. Takes an array reference which should contain one or more rows of
+data, where each row is an array reference.
+
+=item * backend (str, default C<Text::Table::Tiny>)
+
+Optional. Pick a backend module. Available backends:
+
+=over
+
+=item * Text::Table::Tiny
+
+=item * Text::Table::Org
+
+=item * Text::Table::CSV
+
+=item * Text::ANSITable
+
+=item * Text::ASCIITable
+
+=item * Text::FormatTable
+
+=item * Text::MarkdownTable
+
+=item * Text::Table
+
+=item * Text::TabularDisplay
+
+=back
+
+=item * header_row (bool, default 0)
+
+Optional. If given a true value, the first row in the data will be interpreted
+as a header row, and separated visually from the rest of the table (e.g. with a
+ruled line). But some backends won't display differently.
+
+=back
+
+
+=head1 SEE ALSO
+
+L<Bencher::Scenario::TextTableModules>
+
+=cut
